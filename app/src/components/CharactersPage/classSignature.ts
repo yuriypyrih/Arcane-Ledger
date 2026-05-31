bbeafb278813b6c99a68dccc48b3a076f9d5d01a
yuriypyrih <yuriypyrih@gmail.com>
2026-05-31 12:19:42 +0200

    Convert background assets to webp

--- app/src/components/CharactersPage/classSignature.ts
+++ app/src/components/CharactersPage/classSignature.ts
@@ -21,73 +21,73 @@ const defaultClassSignatureStyle: ClassSignatureStyle = {
 };
 
 const classPageTextureByClass: Record<string, () => string> = {
-  artificer: () => new URL("../../assets/img/artificer.jpg", import.meta.url).href,
-  barbarian: () => new URL("../../assets/img/barbarian.jpg", import.meta.url).href,
-  bard: () => new URL("../../assets/img/bard.jpg", import.meta.url).href,
-  cleric: () => new URL("../../assets/img/cleric.jpg", import.meta.url).href,
-  druid: () => new URL("../../assets/img/druid.jpg", import.meta.url).href,
-  fighter: () => new URL("../../assets/img/fighter.jpg", import.meta.url).href,
-  monk: () => new URL("../../assets/img/monk.jpg", import.meta.url).href,
-  paladin: () => new URL("../../assets/img/paladin.jpg", import.meta.url).href,
-  ranger: () => new URL("../../assets/img/ranger.jpg", import.meta.url).href,
-  rogue: () => new URL("../../assets/img/rogue.jpg", import.meta.url).href,
-  sorcerer: () => new URL("../../assets/img/sorcerer.jpg", import.meta.url).href,
-  warlock: () => new URL("../../assets/img/warlock.jpg", import.meta.url).href,
-  wizard: () => new URL("../../assets/img/wizard.jpg", import.meta.url).href
+  artificer: () => new URL("../../assets/img/artificer.webp", import.meta.url).href,
+  barbarian: () => new URL("../../assets/img/barbarian.webp", import.meta.url).href,
+  bard: () => new URL("../../assets/img/bard.webp", import.meta.url).href,
+  cleric: () => new URL("../../assets/img/cleric.webp", import.meta.url).href,
+  druid: () => new URL("../../assets/img/druid.webp", import.meta.url).href,
+  fighter: () => new URL("../../assets/img/fighter.webp", import.meta.url).href,
+  monk: () => new URL("../../assets/img/monk.webp", import.meta.url).href,
+  paladin: () => new URL("../../assets/img/paladin.webp", import.meta.url).href,
+  ranger: () => new URL("../../assets/img/ranger.webp", import.meta.url).href,
+  rogue: () => new URL("../../assets/img/rogue.webp", import.meta.url).href,
+  sorcerer: () => new URL("../../assets/img/sorcerer.webp", import.meta.url).href,
+  warlock: () => new URL("../../assets/img/warlock.webp", import.meta.url).href,
+  wizard: () => new URL("../../assets/img/wizard.webp", import.meta.url).href
 };
 
 const classCornerArtByClass: Record<string, ClassCornerArtSpec> = {
   artificer: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/artificer-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/artificer-corner.webp", import.meta.url).href
   },
   barbarian: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/barbarian-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/barbarian-corner.webp", import.meta.url).href
   },
   bard: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/bard-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/bard-corner.webp", import.meta.url).href
   },
   cleric: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/cleric-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/cleric-corner.webp", import.meta.url).href
   },
   druid: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/druid-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/druid-corner.webp", import.meta.url).href
   },
   fighter: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/fighter-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/fighter-corner.webp", import.meta.url).href
   },
   monk: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/monk-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/monk-corner.webp", import.meta.url).href
   },
   paladin: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/paladin-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/paladin-corner.webp", import.meta.url).href
   },
   ranger: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/ranger-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/ranger-corner.webp", import.meta.url).href
   },
   rogue: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/rogue-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/rogue-corner.webp", import.meta.url).href
   },
   sorcerer: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/sorcerer-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/sorcerer-corner.webp", import.meta.url).href
   },
   warlock: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/warlock-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/warlock-corner.webp", import.meta.url).href
   },
   wizard: {
     getCornerImage: () =>
-      new URL("../../assets/img/class-corners/wizard-corner.png", import.meta.url).href
+      new URL("../../assets/img/class-corners/wizard-corner.webp", import.meta.url).href
   }
 };
 
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ export default tseslint.config(
       "**/*.css",
       "**/*.svg",
       "**/*.png",
+      "**/*.webp",
       "**/*.json",
       "**/*.d.ts",
       ".idea/**",
